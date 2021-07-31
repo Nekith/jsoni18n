2fdbf7f56d4b9d089dabec1e5bf5cbd73d2d615b
Valérian <nekith@errant-works.com>
2021-07-31 15:22:14 +0200

    Change coding style

--- jsoni18n/I18n.hx
+++ jsoni18n/I18n.hx
@@ -2,133 +2,120 @@ package jsoni18n;
 
 import haxe.Json;
 
-class I18n
-{
-    public var depthDelimiter : String = "/";
-    public var varPrefix : String = ":";
-    public var pluralizationVar : String = "_";
+class I18n {
+	public var depthDelimiter:String = "/";
+	public var varPrefix:String = ":";
+	public var pluralizationVar:String = "_";
 
-    private var trads : DynamicObject<Dynamic>;
+	private var trads:DynamicObject<Dynamic>;
 
-    public function new()
-    {
-        trads = new DynamicObject<Dynamic>();
-    }
+	public function new() {
+		trads = new DynamicObject<Dynamic>();
+	}
 
-    public function loadFromString(content : String, ?prefix : String) : Void
-    {
-        var data : DynamicObject<Dynamic> = Json.parse(content);
-        for (key in data.keys()) {
-            var name : String = key;
-            if (prefix != null) {
-                name = prefix + name;
-            }
-            update(trads, name, data.get(key));
-        }
-    }
+	public function loadFromString(content:String, ?prefix:String):Void {
+		var data:DynamicObject<Dynamic> = Json.parse(content);
+		for (key in data.keys()) {
+			var name:String = key;
+			if (prefix != null) {
+				name = prefix + name;
+			}
+			update(trads, name, data.get(key));
+		}
+	}
 
-    public function tr(id : String, ?vars : Map<String, Dynamic>) : String
-    {
-        var str : String = id;
-        if (id.indexOf(depthDelimiter) != -1) {
-            var o : DynamicObject<Dynamic> = fetch(trads, new String(id));
-            if (o != null) {
-                if (Std.is(o, String)) {
-                    str = Std.string(o);
-                } else if (vars != null && vars.exists(pluralizationVar) && o.exists(pluralizationVar)) {
-                    var n : Null<Int> = vars[pluralizationVar];
-                    if (n != null) {
-                        if (n == 0 && o.exists("0")) {
-                            str = o.get("0");
-                        } else if (n == 1 && o.exists("1")) {
-                            str = o.get("1");
-                        } else if (o.exists(pluralizationVar)) {
-                            str = o.get(pluralizationVar);
-                        }
-                    }
-                }
-            }
-        } else if (trads.exists(id) == true) {
-            str = trads.get(id);
-        }
-        if (vars != null) {
-            for (key in vars.keys()) {
-                str = StringTools.replace(str, varPrefix + key, Std.string(vars[key]));
-            }
-        }
-        return str;
-    }
+	public function tr(id:String, ?vars:Map<String, Dynamic>):String {
+		var str:String = id;
+		if (id.indexOf(depthDelimiter) != -1) {
+			var o:DynamicObject<Dynamic> = fetch(trads, new String(id));
+			if (o != null) {
+				if (Std.is(o, String)) {
+					str = Std.string(o);
+				} else if (vars != null && vars.exists(pluralizationVar) && o.exists(pluralizationVar)) {
+					var n:Null<Int> = vars[pluralizationVar];
+					if (n != null) {
+						if (n == 0 && o.exists("0")) {
+							str = o.get("0");
+						} else if (n == 1 && o.exists("1")) {
+							str = o.get("1");
+						} else if (o.exists(pluralizationVar)) {
+							str = o.get(pluralizationVar);
+						}
+					}
+				}
+			}
+		} else if (trads.exists(id) == true) {
+			str = trads.get(id);
+		}
+		if (vars != null) {
+			for (key in vars.keys()) {
+				str = StringTools.replace(str, varPrefix + key, Std.string(vars[key]));
+			}
+		}
+		return str;
+	}
 
-    public function clear() : Void
-    {
-        trads = new DynamicObject<Dynamic>();
-    }
+	public function clear():Void {
+		trads = new DynamicObject<Dynamic>();
+	}
 
-    private function update(el : DynamicObject<Dynamic>, rest : String, data : DynamicObject<Dynamic>) : Void
-    {
-        var pos : Int = rest.indexOf(depthDelimiter);
-        if (pos == -1) {
-            if (el.exists(rest) == true) {
-                el = el.get(rest);
-                for (key in data.keys()) {
-                    el.set(key, data.get(key));
-                }
-            } else {
-                el.set(rest, data);
-            }
-        } else {
-            var part : String = rest.substr(0, pos);
-            rest = rest.substr(pos + 1);
-            var sub : DynamicObject<Dynamic>;
-            if (el.exists(part) == false) {
-                sub = new DynamicObject<Dynamic>();
-                el.set(part, sub);
-            } else {
-                sub = el.get(part);
-            }
-            update(sub, rest, data);
-        }
-    }
+	private function update(el:DynamicObject<Dynamic>, rest:String, data:DynamicObject<Dynamic>):Void {
+		var pos:Int = rest.indexOf(depthDelimiter);
+		if (pos == -1) {
+			if (el.exists(rest) == true) {
+				el = el.get(rest);
+				for (key in data.keys()) {
+					el.set(key, data.get(key));
+				}
+			} else {
+				el.set(rest, data);
+			}
+		} else {
+			var part:String = rest.substr(0, pos);
+			rest = rest.substr(pos + 1);
+			var sub:DynamicObject<Dynamic>;
+			if (el.exists(part) == false) {
+				sub = new DynamicObject<Dynamic>();
+				el.set(part, sub);
+			} else {
+				sub = el.get(part);
+			}
+			update(sub, rest, data);
+		}
+	}
 
-    private function fetch(el : DynamicObject<Dynamic>, rest : String) : DynamicObject<Dynamic>
-    {
-        var pos : Int = rest.indexOf(depthDelimiter);
-        if (pos == -1) {
-            return el.get(rest);
-        }
-        var part : String = rest.substr(0, pos);
-        rest = rest.substr(pos + 1);
-        if (el.exists(part) == false) {
-            return null;
-        }
-        return fetch(el.get(part), rest);
-    }
+	private function fetch(el:DynamicObject<Dynamic>, rest:String):DynamicObject<Dynamic> {
+		var pos:Int = rest.indexOf(depthDelimiter);
+		if (pos == -1) {
+			return el.get(rest);
+		}
+		var part:String = rest.substr(0, pos);
+		rest = rest.substr(pos + 1);
+		if (el.exists(part) == false) {
+			return null;
+		}
+		return fetch(el.get(part), rest);
+	}
 }
 
-abstract DynamicObject<T>(Dynamic<T>) from Dynamic<T>
-{
-    public inline function new()
-    {
-        this = {};
-    }
+abstract DynamicObject<T>(Dynamic<T>) from Dynamic<T> {
+	public inline function new() {
+		this = {};
+	}
 
-    public inline function set(key : String, value : T) : Void
-    {
-        Reflect.setField(this, key, value);
-    }
+	public inline function set(key:String, value:T):Void {
+		Reflect.setField(this, key, value);
+	}
 
-    public inline function get(key : String) : Null<T>
-    {
-        return Reflect.field(this, key);
-    }
+	public inline function get(key:String):Null<T> {
+		return Reflect.field(this, key);
+	}
 
-    public inline function exists(key : String) : Bool
-    {
-        return Reflect.hasField(this, key);
-    }
+	public inline function exists(key:String):Bool {
+		return Reflect.hasField(this, key);
+	}
 
-    public inline function keys() : Array<String>
-    {
-        return Reflect.fields(this);
-    }
+	public inline function keys():Array<String> {
+		return Reflect.fields(this);
+	}
 }
